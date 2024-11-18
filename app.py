9e852ada60d88f3b342c59e4335157b415e42044
Vibish <109983038+VibishnathanG@users.noreply.github.com>
2024-11-18 14:29:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from flask import Flask
+app = Flask(__name__)
 
-app = FastAPI()
+@app.route('/')
+def hello_world():
+    return 'Hello, Docker!'
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+if __name__ == "__main__":
+    # Make sure the host is set to '0.0.0.0' to allow external access
+    app.run(host="0.0.0.0", port=8000)
